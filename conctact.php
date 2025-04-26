7b9ecdb820aa5066135cbf05dca7e41dc77da990
Mallinath4 <115429767+Mallinath4@users.noreply.github.com>
2025-04-26 17:31:47 +0530

    Update conctact.php

--- conctact.php
+++ conctact.php
@@ -6,6 +6,18 @@
     <title>MrigaAayuVets | Contact</title>
     <link rel="stylesheet" href="css/contact.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script>
+   
+
+        // --- 👇 Move your menu-toggle code here ---
+        const menuToggle = document.getElementById('menu-toggle');
+        const navbar = document.getElementById('navbar');
+
+        menuToggle.addEventListener('click', () => {
+            navbar.classList.toggle('show');
+        });
+   
+</script>
 </head>
 <body>
 
